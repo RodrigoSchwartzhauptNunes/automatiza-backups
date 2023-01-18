5c772fc682cd6598e0baed7bac1821c8f6306a74
RodrigoSchwartzhauptNunes <122283957+RodrigoSchwartzhauptNunes@users.noreply.github.com>
2023-01-18 09:03:12 -0300

    Update BACKUP_OLT_HUAWEI.py

--- BACKUP_OLT_HUAWEI.py
+++ BACKUP_OLT_HUAWEI.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
-#=======================================================================
-#By: Rodrigo S Nunes
-#=======================================================================
-#Modo de usar
-#./bk-olt-fiberhome.py {IP_ADDRESS}
-#=======================================================================
-#Bibliotecas
+#====================================================================
+#BACKUP-OLT-HW.py
+#By Rodrigo S Nunes
+#====================================================================
+#Logs
+#Versao nova com repeticao e lista de IPs via API do Zabbix
+#====================================================================
 import sys,pexpect
+from zabbix_api import ZabbixAPI
 import getpass
 import time
+import os
 from datetime import datetime
 today = datetime.now()
 day = today.day
@@ -17,29 +19,56 @@ year = today.year
 hora = today.hour
 min = today.minute
 seg = today.second
-#=======================================================================
-#Argumentos
-HOST = sys.argv[1]
-PATH = sys.argv[2]
-#=======================================================================
-#Variaveis a serem alteradas
-user = 'XXXXXX'
-password = 'XXXXXX'
-FTPSERVER = 'XXXXXX'
-#=======================================================================
+#====================================================================
+#Argumentos USER EQUIPAMENTO
+username = 'vianet'
+password = 'hyu12jmn'
+
+#ZABBIX
+addressz = "http://181.215.211.17"
+usernamez = 'jmnetbkp'
+passwordz = 'CVw65wi*r01g'
+
+#SERVIDOR-FTP
+userftp = 'bkp'
+FTPSERVER = '192.168.100.148'
+
+#====================================================================
+#necessario configurar usuario e senha na olt antes
+#de executar o script
+#====================================================================
 #Main code
-child = pexpect.spawn ('telnet '+HOST)
-child.timeout = 150
-child.logfile = sys.stdout
-time.sleep(2)
-child.sendline (user)
-time.sleep(2)
-child.sendline (password)
-time.sleep(3)
-child.sendline ('EN'+'\r')
-time.sleep(2)
-child.sendline ('backup configuration ftp '+FTPSERVER+' '+PATH+'/'+HOST+'-dia-'+str(day)+'-'+str(month)+'-'+str(year)+'-'+str(hora)+'-'+str(min)+'-'+str(seg)+'.cfg')
-child.sendline ('y'+'\r')
-time.sleep(3)
-child.sendline ('quit')
-child.sendline ('y'+'\r')
+try:
+    zapi = ZabbixAPI(server=addressz); zapi.login(usernamez, passwordz)
+    id_grupo = zapi.hostgroup.get({"search":{"name":"OLT-HW-VIANET"}, "output":["groupid","name"] })
+    nomes_grupo = zapi.host.get({"groupids":id_grupo[0]['groupid'], "output":["host","hostid"] })
+    for y in nomes_grupo:
+            ips_host = zapi.hostinterface.get({"hostids":y['hostid'], "output":["ip"]})
+            for x in ips_host:
+                try:
+                    PATH = x['ip']
+                    print (PATH)
+                    if not os.path.exists('/home/'+userftp+'/'+PATH+''):
+                        os.makedirs('/home/'+userftp+'/'+PATH+'')
+                    try:
+                        child = pexpect.spawn ('ssh '+username+'@'+PATH)
+                        child.timeout = 1500
+                        child.logfile = sys.stdout
+                        time.sleep(10)
+                        child.sendline (password)
+                        time.sleep(3)
+                        child.sendline ('EN'+'\r')
+                        time.sleep(5)
+                        child.sendline ('backup configuration ftp '+FTPSERVER+' '+PATH+'/'+PATH+'-dia-'+str(day)+'-'+str(month)+'-'+str(year)+'-'+str(hora)+'-'+str(min)+'-'+str(seg)+'.cfg')
+                        time.sleep(3)
+                        child.sendline ('y'+'\r')
+                        time.sleep(3)
+                        child.sendline ('quit')
+                        time.sleep(3)
+                        child.sendline ('y'+'\r')
+                    except:
+                        print ("Falha ao acessar equipamento")
+                except:
+                    print("Falha ao criar pasta")
+except:
+    print ("FALHA CODIGO INTEIRO")
